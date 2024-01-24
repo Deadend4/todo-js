5874a59cf8b3a7c8dc35681893eb68dcc4907a0c
Саня <70769398+Deadend4@users.noreply.github.com>
2024-01-24 16:29:17 +0300

    add items left

--- sheet.js
+++ sheet.js
@@ -1,15 +1,24 @@
 const sheet = document.getElementById('sheet__div');
 const sheet__input = document.getElementById('sheet__input');
 const sheet__bottom = document.getElementById('sheet__bottom');
+const sheet__itemsLeft = document.getElementById('sheet__items-left');
 let index = 0;
-function addNode(e) {
+let leftCounter = 0;
+function addNode() {
     const message = document.getElementById('sheet__input');
-    if (index === 0) {
-        sheet__bottom.classList.remove('sheet__bottom_hidden');
-        sheet__input.classList.add('sheet__input_listed');
+    if (message.value !== '') {
+
+        if (index === 0) {
+            sheet__bottom.classList.remove('sheet__bottom_hidden');
+            sheet__input.classList.add('sheet__input_listed');
+        }
+        console.log(message);
+        if (message.value !== '') {
+            createBlock(sheet, message.value);
+        }
+        message.value = '';
     }
-    createBlock(sheet, message.value);
-    message.value = '';
+
 }
 
 function createBlock(root, textMessage) {
@@ -26,6 +35,14 @@ function createBlock(root, textMessage) {
     listCheckbox.type = "checkbox";
     listCheckbox.id = `chechbox${index}`;
     listCheckbox.classList.add('sheet__checkbox');
+    listCheckbox.addEventListener('change', function () {
+        if (this.checked) {
+            leftCounter--;
+        } else {
+            leftCounter++;
+        }
+        updateSheetBottom(sheet__itemsLeft, leftCounter);
+    });
     innerBlock.append(listCheckbox);
     /* label for checkbox */
     const labelCheckbox = document.createElement('label');
@@ -53,6 +70,14 @@ function createBlock(root, textMessage) {
     listDelete.classList.add('sheet__delete');
     listBlock.append(listDelete);
     index++;
+    leftCounter++;
+    updateSheetBottom(sheet__itemsLeft, leftCounter);
+}
+
+function updateSheetBottom(node, counter) {
+    const counterStrings = node.innerHTML.split(' ');
+    counterStrings[0] = counter;
+    node.innerHTML = counterStrings.join(' ');
 }
 
 function destroy_oneListElement(root, list) {
@@ -62,10 +87,11 @@ function destroy_oneListElement(root, list) {
         sheet__bottom.classList.add('sheet__bottom_hidden');
         sheet__input.classList.remove('sheet__input_listed');
     }
-}
+    if (!list.querySelector('.sheet__checkbox').checked) {
+        leftCounter--;
+    }
 
-function delete_parentNode(currentNode) {
-
+    updateSheetBottom(sheet__itemsLeft, leftCounter);
 }
 
 function delete_lastRow(currentNode) {
