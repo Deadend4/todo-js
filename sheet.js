28bf38a1f659a2277a22af211448ba541d87e50d
Deadend4 <vistanciya.toxin@mail.ru>
2024-01-26 20:01:57 +0300

    fix element names

--- sheet.js
+++ sheet.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-const sheet = document.getElementById('sheet__div');
+const sheet = document.getElementById('list');
 const sheetInput = document.getElementById('sheet__input');
 const sheetBottom = document.getElementById('sheet__bottom');
 const sheetItemsLeft = document.getElementById('sheet__items-left');
@@ -17,137 +17,141 @@ let leftCounter = 0;
 let sheetToggles = [];
 
 sheetToggleAllLabel.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleAll();
-    return false;
+  e.preventDefault();
+  toggleAll();
+  return false;
 });
 
 sheetClearComplete.addEventListener('click', (e) => {
-    e.preventDefault();
-    clearCompleted(sheet, sheetToggles);
-    return false;
-})
+  e.preventDefault();
+  clearCompleted(sheet, sheetToggles);
+  return false;
+});
 
 function toggleAll() {
-    sheetToggles = document.querySelectorAll('.sheet__checkbox');
-    sheetToggleAll.checked = leftCounter > 0 && (leftCounter === sheetToggles.length || !sheetToggleAll.checked);
-    sheetToggles.forEach((item) => {
-        item.checked = sheetToggleAll.checked;
-    });
-    leftCounter = sheetToggleAll.checked ? 0 : sheetToggles.length;
-    showHideClearComplete(sheetToggles.length, leftCounter);
-    updateLeftItems(sheetItemsLeft, leftCounter);
+  sheetToggles = document.querySelectorAll('.list__checkbox');
+  sheetToggleAll.checked =
+    leftCounter > 0 &&
+    (leftCounter === sheetToggles.length || !sheetToggleAll.checked);
+  sheetToggles.forEach((item) => {
+    item.checked = sheetToggleAll.checked;
+  });
+  leftCounter = sheetToggleAll.checked ? 0 : sheetToggles.length;
+  showHideClearComplete(sheetToggles.length, leftCounter);
+  updateLeftItems(sheetItemsLeft, leftCounter);
 }
 
 function addNode() {
-    if (sheetInput.value !== '') {
-        if (index === 0) {
-            sheetBottom.classList.remove('sheet__bottom_hidden');
-            sheetInput.classList.add('sheet__input_listed');
-            sheetToggleAllLabel.classList.remove('sheet__toggle-all-label_hidden');
-        }
-        if (sheetInput.value !== '') {
-            createBlock(sheet, sheetInput.value);
-        }
-        sheetInput.value = '';
+  if (sheetInput.value !== '') {
+    if (index === 0) {
+      sheetBottom.classList.remove('sheet__bottom_hidden');
+      sheetInput.classList.add('sheet__input_listed');
+      sheetToggleAllLabel.classList.remove('sheet__toggle-all-label_hidden');
     }
+    if (sheetInput.value !== '') {
+      createBlock(sheet, sheetInput.value);
+    }
+    sheetInput.value = '';
+  }
 }
 
-function createBlock(root, textsheetInput) {
-    /* the main container for a list item */
-    const listBlock = document.createElement('div');
-    listBlock.classList.add('sheet__list');
-    root.prepend(listBlock);
-    /* a flex block for a list item content */
-    const innerBlock = document.createElement('div');
-    innerBlock.classList.add('sheet__inner-list');
-    listBlock.prepend(innerBlock);
-    /* checkbox */
-    const listCheckbox = document.createElement('input');
-    listCheckbox.type = "checkbox";
-    listCheckbox.id = `chechbox${index}`;
-    listCheckbox.classList.add('sheet__checkbox');
-    listCheckbox.addEventListener('change', function () {
-        if (this.checked) {
-            leftCounter--;
-        } else {
-            leftCounter++;
-        }
-        sheetToggles = document.querySelectorAll('.sheet__checkbox');
-        showHideClearComplete(sheetToggles.length, leftCounter);
-        updateLeftItems(sheetItemsLeft, leftCounter);
-    });
-    innerBlock.append(listCheckbox);
-    /* label for checkbox */
-    const labelCheckbox = document.createElement('label');
-    labelCheckbox.innerHTML = '';
-    labelCheckbox.classList.add('sheet__checkbox-label');
-    labelCheckbox.htmlFor = `chechbox${index}`;
-    innerBlock.append(labelCheckbox);
-    /* todo text  */
-    const listText = document.createElement('label');
-    listText.classList.add('sheet__label');
-    listText.innerHTML = textsheetInput;
-    listText.contentEditable = true;
-    innerBlock.append(listText);
-
-    const listDelete = document.createElement('button');
-
-    listDelete.innerHTML = '×';
-    listDelete.type = 'button';
-    listDelete.addEventListener('click', (e) => {
-        e.preventDefault();
-        destroyOneListElement(sheet, listBlock);
-        return false;
-    });
-    listDelete.classList.add('sheet__delete');
-    listBlock.append(listDelete);
-    /*update ui*/
-    index++;
-    leftCounter++;
+function createBlock(root, textSheetInput) {
+  /* the main container for a list item */
+  const listBlock = document.createElement('div');
+  listBlock.classList.add('list__item');
+  root.prepend(listBlock);
+  /* a flex block for a list item content */
+  const innerBlock = document.createElement('div');
+  innerBlock.classList.add('list__item-content');
+  listBlock.prepend(innerBlock);
+  /* checkbox */
+  const listCheckbox = document.createElement('input');
+  listCheckbox.type = 'checkbox';
+  listCheckbox.id = `chechbox${index}`;
+  listCheckbox.classList.add('list__checkbox');
+  listCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+      leftCounter--;
+    } else {
+      leftCounter++;
+    }
+    sheetToggles = document.querySelectorAll('.list__checkbox');
+    showHideClearComplete(sheetToggles.length, leftCounter);
     updateLeftItems(sheetItemsLeft, leftCounter);
+  });
+  innerBlock.append(listCheckbox);
+  /* label for checkbox */
+  const labelCheckbox = document.createElement('label');
+  labelCheckbox.innerHTML = '';
+  labelCheckbox.classList.add('list__checkbox-label');
+  labelCheckbox.htmlFor = `chechbox${index}`;
+  innerBlock.append(labelCheckbox);
+  /* todo text  */
+  const listText = document.createElement('label');
+  listText.classList.add('list__item-text');
+  listText.innerHTML = textSheetInput;
+  listText.contentEditable = true;
+  innerBlock.append(listText);
+
+  const listDelete = document.createElement('button');
+
+  listDelete.innerHTML = '×';
+  listDelete.type = 'button';
+  listDelete.addEventListener('click', (e) => {
+    e.preventDefault();
+    destroyOneListElement(sheet, listBlock);
+    return false;
+  });
+  listDelete.classList.add('list__delete-item');
+  listBlock.append(listDelete);
+  /*update ui*/
+  index++;
+  leftCounter++;
+  updateLeftItems(sheetItemsLeft, leftCounter);
 }
 
 function updateLeftItems(node, counter) {
-    const counterStrings = node.innerHTML.split(' ');
-    counterStrings[0] = counter;
-    node.innerHTML = counterStrings.join(' ');
+  const counterStrings = node.innerHTML.split(' ');
+  counterStrings[0] = counter;
+  node.innerHTML = counterStrings.join(' ');
 }
 
 function switchFilters() {
-    /*in progress*/
+  /*in progress*/
 }
 
 function showHideClearComplete(length, counter) {
-    if (counter < length) {
-        sheetClearComplete.classList.remove('sheet__clear-complete-label_hidden');
-    } else {
-        sheetClearComplete.classList.add('sheet__clear-complete-label_hidden');
-    }
+  if (counter < length) {
+    sheetClearComplete.classList.remove('sheet__clear-complete-label_hidden');
+  } else {
+    sheetClearComplete.classList.add('sheet__clear-complete-label_hidden');
+  }
 }
 
 function clearCompleted(root, checkboxes) {
-    checkboxes.forEach((item) => {
-        if (item.checked) {
-            destroyOneListElement(root, item.parentNode.parentNode);
-        }
-    })
+  checkboxes.forEach((item) => {
+    if (item.checked) {
+      destroyOneListElement(root, item.parentNode.parentNode);
+    }
+  });
 }
 
 function destroyOneListElement(root, list) {
-    root.removeChild(list);
-    if (root.lastElementChild === null) {
-        index = 0;
-        sheetBottom.classList.add('sheet__bottom_hidden');
-        sheetInput.classList.remove('sheet__input_listed');
-        sheetToggleAllLabel.classList.add('sheet__toggle-all-label-hidden');
-    }
-    if (!list.querySelector('.sheet__checkbox').checked) {
-        leftCounter--;
-    }
-    updateLeftItems(sheetItemsLeft, leftCounter);
+  root.removeChild(list);
+  if (root.lastElementChild === null) {
+    index = 0;
+    sheetBottom.classList.add('sheet__bottom_hidden');
+    sheetInput.classList.remove('sheet__input_listed');
+    sheetToggleAllLabel.classList.add('sheet__toggle-all-label-hidden');
+  }
+  if (!list.querySelector('.list__checkbox').checked) {
+    leftCounter--;
+  }
+  updateLeftItems(sheetItemsLeft, leftCounter);
 }
 
 function destroyLastRow(currentNode) {
-    currentNode.parentNode.parentNode.removeChild(currentNode.parentNode.parentNode.lastElementChild);
+  currentNode.parentNode.parentNode.removeChild(
+    currentNode.parentNode.parentNode.lastElementChild
+  );
 }
